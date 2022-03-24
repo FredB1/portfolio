ebb46b9f7b4d6fb86d0de0af86e47b1e16b57b47
FredB1 <fredb1>
2022-03-24 18:35:17 -0400

    branding

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <div class="brand">
+        <h1>FRED BURKE</h1>
+        <h4>FULL STACK WEB DEVELOPER</h4>
+      </div>
+    </nav>
+  );
+};
+export default Header;
