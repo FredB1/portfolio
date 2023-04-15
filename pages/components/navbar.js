e950a3a87f2afa0cfc5a95185f4556de8735d1e8
root <root@mx>
2023-04-15 15:20:28 -0400

    clean tree

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import Link from 'next/link';
+import React, { useState } from 'react';
+import Link from 'next/link'; 
+import { FiMenu } from 'react-icons/fi';
+
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div>
-            <nav class="bg-blue-500 text-black">
+            <nav class="bg-blue-500 text-black ">
                 <div class="container mx-auto px-6 py-4 flex items-center">
                     <Link href="/" class="text-5xl font-semibold font-Bangers tracking-custom">Fred Burke</Link>
                     <div class="ml-auto flex items-center">
@@ -12,7 +16,7 @@ const Navbar = () => {
                         <Link href="/projects" class="px-3 py-2 rounded-md text-3xl font-medium leading-5 text-black-700 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Projects</Link>
                         <Link href="/contact" class="px-3 py-2 rounded-md text-3xl font-medium leading-5 text-black-700 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Contact</Link>
                     </div>
-                    <div class="ml-auto flex">
+                    <div class={`md:hidden ${isOpen ? 'flex ml-auto' : 'hidden'}`}>
                         <a href="https://www.github.com/fredb1" target="_blank" rel="noopener noreferrer" alt="My Github">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#171515" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -20,26 +24,44 @@ const Navbar = () => {
                             </svg>
                         </a>
                         <a href="https://www.linkedin.com/in/fredb7" target="_blank" rel="noopener noreferrer" alt="Connect with me on Linkedin">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#171515" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <rect x="4" y="4" width="16" height="16" rx="2" />
-                            <line x1="8" y1="11" x2="8" y2="16" />
-                            <line x1="8" y1="8" x2="8" y2="8.01" />
-                            <line x1="12" y1="16" x2="12" y2="11" />
-                            <path d="M16 16v-3a2 2 0 0 0 -4 0" />
-                        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#171515" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <rect x="4" y="4" width="16" height="16" rx="2" />
+                                <line x1="8" y1="11" x2="8" y2="16" />
+                                <line x1="8" y1="8" x2="8" y2="8.01" />
+                                <line x1="12" y1="16" x2="12" y2="11" />
+                                <path d="M16 16v-3a2 2 0 0 0 -4 0" />
+                            </svg>
                         </a>
                         <a href="../FredBurkeResume315.pdf" download="FredBurkeResume315.pdf">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-export" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#171515" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-                            <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v5m-5 6h7m-3 -3l3 3l-3 3" />
-                        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-export" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#171515" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                                <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v5m-5 6h7m-3 -3l3 3l-3 3" />
+                            </svg>
                         </a>
                     </div>
                 </div>
             </nav>
-
+            <button
+                type="button"
+                onClick={() => setIsOpen(!isOpen)}
+                class="block md:hidden">
+                <FiMenu class={`w-6 h-6 ${isOpen ? 'hidden' : 'block'}`} />
+                <svg
+                    class={`w-6 h-6 ${isOpen ? 'block' : 'hidden'}`}
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                    />
+                </svg>
+            </button>
         </div>
     )
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/navbar'
 import ReCAPTCHA from 'react-google-recaptcha';
+import { send } from '@emailjs/browser';
 
 const Contact = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [recaptchaToken, setRecaptchaToken] = useState('');
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+    const [canSubmit, setCanSubmit] = useState(false);
 
+
+    useEffect(() => {
+        setCanSubmit(canSubmitForm());
+      }, []);
+      
     const handleNameChange = (e) => {
         setName(e.target.value);
     };
@@ -23,39 +31,59 @@ const Contact = () => {
     const handleRecaptchaChange = (token) => {
         setRecaptchaToken(token);
     };
+    const isClientSide = () => typeof window !== 'undefined';
+    const canSubmitForm = () => {
+        if (!isClientSide()) {
+            return false;
+        }
+        const lastSubmissionTime = localStorage.getItem('lastSubmissionTime');
+        const timeDifferenceInHours =
+            (Date.now() - (lastSubmissionTime || 0)) / 1000 / 60 / 60;
+
+        return timeDifferenceInHours >= 5; // Replace 2 with the number of hours you want
+    };
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-    
+
         if (recaptchaToken) {
-          const response = await fetch('/api/send-email', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name,
-                email,
-                message,
-                recaptchaToken,
-            }),
-          });
-    
-          const data = await response.json();
-    
-          console.log(data);
+            const serviceId = 'service_8brirop';
+            const templateId = 'template_wuxi2kc';
+            const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+            const templateParams = {
+                from_name: name,
+                reply_to: email,
+                message: message,
+            };
+
+            try {
+                const result = await send(serviceId, templateId, templateParams, publicKey);
+                console.log(result.text);
+                setIsFormSubmitted(true);
+                if (isClientSide()) {
+                    localStorage.setItem('lastSubmissionTime', Date.now());
+                }
+            } catch (error) {
+                console.log(error);
+            }
         } else {
-          alert('Please complete the recaptcha.');
+            alert('Please complete the recaptcha.');
         }
-      };
-    
+    };
+
 
     return (
         <div>
             <Navbar />
             <div className="container mx-auto px-4 py-12">
                 <h2 className="text-3xl font-semibold mb-6">Get in touch</h2>
-                <form onSubmit={handleSubmit}>
+                {isFormSubmitted ? (
+                    <p className="text-xl text-green-600 bg-white w-full p-4 max-w-prose">
+                        Thank you! Your message has been sent successfully.
+                    </p>
+                ) : canSubmit ? (<form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="name" className="block text-gray-700 mb-2">
                             Name
@@ -92,7 +120,7 @@ const Contact = () => {
                             className="w-full border-2 border-gray-300 p-2 rounded"
                         ></textarea>
                     </div>
-                    <ReCAPTCHA 
+                    <ReCAPTCHA
                         sitekey="6LfHrnYlAAAAADRJ2xalnoEbbitFPlqJsugg4qxr"
                         onChange={handleRecaptchaChange}
                         render={(props) => <div {...props} />}
@@ -103,7 +131,11 @@ const Contact = () => {
                     >
                         Send
                     </button>
-                </form>
+                </form>) : (
+                    <p className="text-xl text-red-600 bg-white w-full max-w-prose p-4">
+                        You have already submitted a message, please try again later.
+                    </p>
+                )}
             </div>
         </div>
     );
