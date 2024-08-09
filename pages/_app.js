7fd6f9c07a379337ea63c74c6004862ecb1cece9
Fred Burke <fburke77@gmail.com>
2024-08-09 11:25:56 -0400

    fixing metadata

--- pages/_app.js
+++ pages/_app.js
@@ -13,13 +13,13 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Welcome to my awesome site." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
-        <meta property="og:title" content="%APP_ID%" />
+        <meta property="og:title" content={process.env.NEXT_PUBLIC_APP_ID || process.env.APP_ID} />
         <meta property="og:description" content="Welcome to my site." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="%PUBLIC_URL%" />
         <meta property="og:image" content="/default-og-image.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="%APP_ID%" />
+        <meta name="twitter:title" content={process.env.NEXT_PUBLIC_APP_ID || process.env.APP_ID} />
         <meta name="twitter:description" content="Welcome to my site." />
         <meta name="twitter:image" content="/default-twitter-image.jpg" />
       </Head>
