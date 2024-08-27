1255bbee8fa19014e1e43a66328e2fc27f229b09
Fred Burke <fburke77@gmail.com>
2024-08-27 05:57:25 -0400

    chore: Update _app.js to include HTML5 doctype and lang attribute

--- pages/_app.js
+++ pages/_app.js
@@ -9,33 +9,36 @@ function MyApp({ Component, pageProps }) {
     ? `${pageProps.title} | ${process.env.NEXT_PUBLIC_APP_ID}`
     : process.env.NEXT_PUBLIC_APP_ID;
   return (
-    <html lang="en">
-      <>
-        ``
-        <Head>
-          <title>{title}</title>
-          <meta name="description" content="Welcome to my awesome site." />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta charSet="UTF-8" />
-          <meta property="og:title" content={process.env.NEXT_PUBLIC_APP_ID || process.env.APP_ID} />
-          <meta property="og:description" content="Welcome to my site." />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="%PUBLIC_URL%" />
-          <meta property="og:image" content="/default-og-image.jpg" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={process.env.NEXT_PUBLIC_APP_ID || process.env.APP_ID} />
-          <meta name="twitter:description" content="Welcome to my site." />
-          <meta name="twitter:image" content="/default-twitter-image.jpg" />
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        <GoogleTagManager gtmId="AW-803265503" />
+    <>
+      ``
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="Welcome to my awesome site." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta property="og:title" content={process.env.NEXT_PUBLIC_APP_ID || process.env.APP_ID} />
+        <meta property="og:description" content="Welcome to my site." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="%PUBLIC_URL%" />
+        <meta property="og:image" content="/default-og-image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={process.env.NEXT_PUBLIC_APP_ID || process.env.APP_ID} />
+        <meta name="twitter:description" content="Welcome to my site." />
+        <meta name="twitter:image" content="/default-twitter-image.jpg" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_G_TAG_MANAGER} />
 
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=AW-803265503"
-          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
-      </>
-    </html>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_G_TAG_MANAGER}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>   </>
   );
 }
 
